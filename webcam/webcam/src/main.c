d33c55aa7a200428b2c902cbaa077def9d4ce5ab
Nathan Shelly <nsnshelly@gmail.com>
2017-06-09 22:27:40 -0500

    main styling changes

--- webcam/webcam/src/main.c
+++ webcam/webcam/src/main.c
@@ -14,27 +14,30 @@ int main (void)
 	tc_start(TC0, 0);
 		
 	// Custom configuration calls
-	configure_wifi();	// configures and initializes wifi 
-	configure_camera();	// configures and initializes camera module
+	configure_wifi();
+	configure_camera();
 	
 	reboot_wifi();
 	
-	configure_i2s(); // microphone configuration
+	// microphone configuration
+	configure_i2s();
+	// open initial websocket
 	configure_websocket();
 	
 	start_i2s_capture();
 	
 	uint32_t im_length;
 	while(1) {
-		if(wifi_setup_flag) {	// if the user pressed the wifi setup button, 
-			setup_wifi();		// the wifi chip tries to reassociate to a new network
-		}
+		// trigger setup to new network when setup button pressed
+		if(wifi_setup_flag)
+			setup_wifi();
 		
-		start_capture();		// captures image to internal memory. Owned by camera but sends audio data
+		// captures image to internal memory
+		// Owned by camera but sends audio data
+		start_capture();
 		im_length = find_image_len();
-		if(im_length) {
+		if(im_length)
 			send_image(start_of_image_ptr, im_length);
-		}
 	}
 	return 0;
 }
